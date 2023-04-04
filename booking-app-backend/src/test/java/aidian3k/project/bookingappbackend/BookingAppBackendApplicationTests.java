7e48c6e375fdf75d60b3c4a88d7f093b097cb80a
aidian3k <adrian.nowosielski1@gmail.com>
2023-04-04 22:37:00 +0200

    Added raw project

---
+++ booking-app-backend/src/test/java/aidian3k/project/bookingappbackend/BookingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package aidian3k.project.bookingappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
